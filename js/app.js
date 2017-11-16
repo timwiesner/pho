d8106cb9b2172f1007e842edcea29fbed5698d18
timwiesner <timothyjwiesner@gmail.com>
2017-11-16 10:23:13 -0600

    feat: Add back-to-top button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
-// // Only enable if the document has a long scroll bar
-// // Note the window height + offset
-// if ( ($(window).height() + 100) < $(document).height() ) {
-//     $('#top-link-block').removeClass('hidden').affix({
-//         // how far to scroll down before link "slides" into view
-//         offset: {top:100}
-//     });
-// }
+$(document).ready(function() {
+	var offset = 250;
+	var duration = 300;
+
+
+$(window).scroll(function() {
+	if ($(this).scrollTop() > offset) {
+		$('.back-to-top').fadeIn(duration);
+	} else {
+		$('.back-to-top').fadeOut(duration);
+	}
+});
+
+
+
+$('.back-to-top').click(function(event) {
+	event.preventDefault();
+	$('html, body').animate({scrollTop: 0}, duration);
+	return false;
+	})
+});
